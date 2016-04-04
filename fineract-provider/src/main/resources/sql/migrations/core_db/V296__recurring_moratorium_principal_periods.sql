1d53c99fa36cb305b5050c046c96a6ba1c56f764
binnygopinath <binny.gopinath@gmail.com>
2016-04-04 18:02:33 +0530

    FINERACT-94: Fixed integration test issues

--- fineract-provider/src/main/resources/sql/migrations/core_db/V296__recurring_moratorium_principal_periods.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE m_product_loan
-	ADD COLUMN `recurring_moratorium_principal_periods` SMALLINT(5) AFTER `grace_on_principal_periods`;
-	
-ALTER TABLE m_loan
-	ADD COLUMN `recurring_moratorium_principal_periods` SMALLINT(5) AFTER `grace_on_principal_periods`;
